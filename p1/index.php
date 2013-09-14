69078345c242c5e996aec71cfd66606b95fbd747
dlafevor <dlafevor@g.harvard.edu>
2013-09-14 17:09:04 -0500

    first commit - base index.php and master.css

---
+++ p1/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Bio &ndash; Daniel LaFevor</title>
+   	<link href="styles/master.css" rel="stylesheet" type="text/css">
+  </head>
+  
+<body>
+  <div class="leftColumn">
+    <h1>Daniel<br>LaFevor</h1>
+    <ul>
+    	<li></li>
+    </ul>
+  </div>
+	<div class="body">
+  	<p>Hello, my name is Daniel LaFevor. I am a  Web Developer living in Middle Tennessee.</p>
+    <p>I primarily develop with Adobe ColdFusion, Microsoft SQL, and </p>
+  </div>  	
+</body>
+</html>
