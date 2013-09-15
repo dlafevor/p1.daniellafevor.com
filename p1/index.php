ee8177203d1b42460b56e21d1e1f947f7ddb9900
dlafevor <dlafevor@g.harvard.edu>
2013-09-14 21:31:14 -0500

    cleaned up styles and changed page layout ... also tweaked the text on the index.php

--- p1/index.php
+++ p1/index.php
@@ -7,27 +7,33 @@
   </head>
   
 	<body>
-  	<div class="leftColumn">
-     	<h1>Daniel LaFevor</h1>
-    	<img src="images/photoofme.png" width="150" height="124" alt="Photo of me" title="photo of me" class="photoOfMe">
-      <ul>
-        <li>Sysem Development</li>
-        <li>Interface Design</li>
-        <li>Usability</li>
-        <li>Interactive Design</li>
-        <li>Art Direction</li>
-        <li>Graphic Design</li>
-        <li>Illustration</li>
-        <li>Photography</li>
-      </ul>
-    </div>
-    <div class="bodySection">
-        <h2>About</h2>
-        <p>Hello, my name is Daniel LaFevor and I live in middle Tennessee (about 45 minutes southeast of Nashville). I am married with two dogs &ndash; an Airedale terrier named Murphy and a schnauzer named Siri (not the app, but short for Sirius the dog star). In my spare time I enjoy drawing, SCCA racing, and hiking.</p>
-        <h2>Work</h2>
-        <p>I am a Web Developer working for Jacobs Engineering (Technology Region). I developed, with the help Linda Curty (a fellow student in this class), a the system our region uses for managing bid and contract data. It is built in Adobe ColdFusion, Microsoft SQL and jQuery. I am looking forward to learning PHP. I have very little experience with it, and look forward to learning more (the same goes for mySQL).</p>
-        <h2>Code Editor</h2>
-        <p>Adobe Dreamweaver CC or Adobe Dreamweaver 5 depending on if I use my laptop from work or if I am working at home.</p>
-    </div>
+     <div id="pageHeader">
+     	CSCI E-15 Project 1
+     </div>
+     <div id="wrapper">
+      <div class="leftColumn">
+        <h1>Daniel LaFevor</h1>
+        <img src="images/photoofme.png" width="150" height="124" alt="Photo of me" title="photo of me" class="photoOfMe">
+        <ul>
+          <li>Sysem Development</li>
+          <li>Interface Design</li>
+          <li>Usability</li>
+          <li>Interactive Design</li>
+          <li>Art Direction</li>
+          <li>Graphic Design</li>
+          <li>Illustration</li>
+          <li>Photography</li>
+        </ul>
+        <hr class="clearLeft">
+      </div>
+      <div class="bodySection">
+          <h2>About</h2>
+          <p>Hello, my name is Daniel LaFevor and I live in middle Tennessee, about 45 minutes southeast of Nashville. I am married with two dogs and no kids. In my spare time I enjoy drawing, SCCA racing, and hiking with Murphy my Airedale terrier.</p>
+          <h2>Work</h2>
+          <p>I am a Web Developer working for Jacobs Engineering (Technology Region). I developed, with the help Linda Curty (a fellow student in this class), the system our region uses for managing bid and contract data. It is built in Adobe ColdFusion, Microsoft SQL and jQuery. I am looking forward to learning PHP. I have very little experience with it, and look forward to learning more (the same goes for mySQL).</p>
+          <h2>Code Editor</h2>
+          <p>Adobe Dreamweaver CC or Adobe Dreamweaver 5 depending on if I use my laptop from work or if I am working at home.</p>
+      </div>
+     </div>
   </body>
 </html>
